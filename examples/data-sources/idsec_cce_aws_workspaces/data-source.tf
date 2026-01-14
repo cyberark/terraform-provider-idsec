ace85f5aab67b6081b9b54249d3bf4bed358b1e2
CyberarkReleaser <svc_go_ark_releaser@cyberark.com>
2026-01-14 15:49:25 +0000

    OLY-00000: remove cce examples

--- examples/data-sources/idsec_cce_aws_workspaces/data-source.tf
+++
@@ -1,8 +0,0 @@
-data "idsec_cce_aws_workspaces" "all_workspaces" {
-  # Optional filters:
-  # include_suspended = true
-  # workspace_type = "aws_account"
-  # workspace_status = "Completely added"
-  # parent_id = "aaaa1111bbbb2222cccc3333dddd4444"
-  # services = "dpa,sca"
-}
